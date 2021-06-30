73b8f74612b9f124dc68a47c50f15a1c2b76f386
Rene BA <amador_19927@hotmail.com>
2021-06-29 19:31:23 -0500

    elimiann

--- nodesuma/suma.js
+++
@@ -1,5 +0,0 @@
-function suma(a,b){
-    return parseInt(a) + parseInt(b);
-}
-
-module.exports = suma;
--- nodesuma/suma.test.js
+++
@@ -1,5 +0,0 @@
-const suma = require('./suma');
-
-test('1 + 1 = 2', () => {
-    expect(suma(1,1)).toBe(2);
-});
